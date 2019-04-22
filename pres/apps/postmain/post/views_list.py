a0a41b6d31e50ef9a5aed8d3f10bcf06f4862821
viniciusroland <vini_roland4.10@hotmail.com>
2019-04-22 11:36:38 -0300

    fixing template name

--- pres/apps/postmain/post/views_list.py
+++ pres/apps/postmain/post/views_list.py
@@ -4,8 +4,9 @@ from django.views import View
 # Create your views here.
 
 class FeedGeneral(View):
+    template_name = 'postmain/post/feedgeneral.html'
     def get(self, request):
-        return render(request, 'postmain/post/feedgeneral.html')
+        return render(request, self.template_name)
 
 
     def post(self, request):
